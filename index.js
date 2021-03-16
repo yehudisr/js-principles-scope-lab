751c3c8544eea76a8be11f017e2edeca298b1c9d
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-16 09:36:31 -0400

    commit tree 330685f6ff2919cd7ddce596b65b3049c58d0582

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'me'
+
+function upperCaseCustomerName(name){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'you'
+}
